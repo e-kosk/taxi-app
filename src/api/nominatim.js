4317a43fbf43ab64ba6c5520b24412c9957c5130
wegrzynmaciej <wegrzyn.maciej@tlen.pl>
2022-04-19 23:35:09 +0200

    implemented get address from coords

--- src/api/nominatim.js
+++ src/api/nominatim.js
@@ -1,5 +1,31 @@
-export function getAddressFromLatLon(lat, lon) {
-    return 'Długa 5'
+// export function getAddressFromLatLon(lat, lon) {
+//     const Http = new XMLHttpRequest();
+//     const url='http://nominatim.openstreetmap.org/reverse?format=json&lat='+lat+'&lon='+lon+'&zoom=27&addressdetails=1&accept-language=PL_pl';
+//     Http.open("GET", url);
+//     Http.send();
+//     Http.onreadystatechange = (e) => {
+//       var data['address'] = JSON.parse(Http.responseText)['address'];
+//       var address = data['address']['road'] + ' ' + data['address']['house_number'] + ', ' + data['address']['postcode'] + ' ' + data['address']['county'];
+//       callbackAddress(address);
+//     }
+// }
+
+async function getAddress(lat, lon) {
+    let url = 'http://nominatim.openstreetmap.org/reverse?format=json&lat='+lat+'&lon='+lon+'&zoom=27&addressdetails=1&accept-language=PL_pl';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export async function getAddressFromLatLon(lat, lon) {
+    let address = await getAddress(lat, lon);
+    console.log(address);
+    document.getElementById('address_from').value=`${address.address.road} ${address.address.house_number}, ${address.address.postcode} ${address.address.county}`;
+    return true;
 }
 
 export function getLatLonFromAddress(address) {
