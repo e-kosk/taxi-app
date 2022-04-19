2755f632f166623e1521714edf19ac7cce764863
Maciej Węgrzyn <darthmusk@gmail.com>
2022-04-19 13:34:41 +0200

    firebase implementation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Router, Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home'
 import About from './pages/About'
 import Trip from './pages/Trip'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Reset from './pages/Reset'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/reset" element={<Reset />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route path="home" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="trip" element={<Trip />} />
       </Routes>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,24 @@
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import styles from "./Layout.module.scss";
+import { logout } from "../firebase";
+
 
 function Layout({children}) {
+    let navigate = useNavigate(); 
+    const logout_redirect = () =>{ 
+    let path = `/`; 
+    logout();
+    navigate(path);
+  }
+
     return (
         <div className={styles.layout}>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/home">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
+                    <button className="dashboard__btn" onClick={logout_redirect}>Logout</button>
                 </ul>
             </nav>
             <div className={styles.content}>
