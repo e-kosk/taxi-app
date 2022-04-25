4879545a2955b75ebb067860fb0160ab8ab94d72
Maciej Węgrzyn <darthmusk@gmail.com>
2022-04-25 12:34:51 +0200

    Implemented sound&vibration

--- src/components/DriverArrived.jsx
+++ src/components/DriverArrived.jsx
@@ -5,9 +5,21 @@ import styles from "./Trip.module.scss";
 import CarCrashIcon from "@mui/icons-material/CarCrash";
 import { useState, useEffect, useContext } from "react";
 import { MapContext } from "../context/MapContext";
+import UIFx from "uifx";
+import noti from '../sounds/noti.mp3';
+
+
 
 const DriverArrived = ({ address, startTrip, name, car }) => {
   const {initial, from, setFrom, to, setTo, userLocation, setUserLocation} = useContext(MapContext);
+  const alert = new UIFx(
+    noti, 
+    {
+      volume: 0.4, // number between 0.0 ~ 1.0
+      throttleMs: 100
+    });
+  alert.play();
+  navigator.vibrate(200,50,200);
   return (
     <div className={styles.driverArrived}>
       <h3>
