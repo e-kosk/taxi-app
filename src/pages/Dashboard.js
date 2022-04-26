c94e08e2fccb7b238c23a2fd8ebd98294de2f2b7
wegrzynmaciej <wegrzyn.maciej@tlen.pl>
2022-04-26 23:51:29 +0200

    SW + Messaging fix

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,19 +3,23 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
 import { auth, db, logout } from "../firebase";
-import { query, collection, getDocs, where } from "firebase/firestore";
+import { query, collection, getDoc, doc, where } from "firebase/firestore";
 function Dashboard() {
   const [user, loading, error] = useAuthState(auth);
   const [name, setName] = useState("");
   const navigate = useNavigate();
   const fetchUserName = async () => {
     try {
-      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
-      const doc = await getDocs(q);
-      const data = doc.docs[0].data();
+      
+      const q = doc(db, "users", user.uid);
+
+      const docs = await getDoc(q);
+      const data = docs.data();
+
       setName(data.name);
     } catch (err) {
       console.error(err);
+
       alert("An error occured while fetching user data");
     }
   };
