4901b42310b27ca9893aa510f674f4749dee30fe
wegrzynmaciej <wegrzyn.maciej@tlen.pl>
2022-04-26 21:50:59 +0200

    maphook

--- src/components/MapHooks.jsx
+++ src/components/MapHooks.jsx
@@ -49,7 +49,7 @@ const CreateRoutineMachineLayer = (props) => {
         var routes = e.routes;
         var summary = routes[0].summary;
         // alert distance and time in km and minutes
-        alert('Total distance is ' + summary.totalDistance / 1000 + ' km and total time is ' + Math.round(summary.totalTime % 3600 / 60) + ' minutes');
+        var cost =  (summary.totalDistance / 1000) * 1.80;
     });
     return instance;
 };
